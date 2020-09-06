542b9513e2b58dd2617a6b704fe4f3537e8ec1e4
natainakata <satzin0521@gmail.com>
2020-09-06 15:08:13 +0900

    reformat Code

--- src/main/kotlin/io/github/natainakata/mineslayer/MineSlayerMain.kt
+++ src/main/kotlin/io/github/natainakata/mineslayer/MineSlayerMain.kt
@@ -10,75 +10,75 @@ import net.minecraftforge.fml.common.event.FMLPreInitializationEvent
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent
 
 @Mod(
-    modid = MineSlayerMain.MOD_ID,
-    name = MineSlayerMain.MOD_NAME,
-    version = MineSlayerMain.VERSION,
-    modLanguageAdapter = "net.shadowfacts.forgelin.KotlinAdapter"
+  modid = MineSlayerMain.MOD_ID,
+  name = MineSlayerMain.MOD_NAME,
+  version = MineSlayerMain.VERSION,
+  modLanguageAdapter = "net.shadowfacts.forgelin.KotlinAdapter"
 )
 object MineSlayerMain {
-    const val MOD_ID = "mineslayer"
-    const val MOD_NAME = "MineSlayer"
-    const val VERSION = "alpha-1.0.0"
+  const val MOD_ID = "mineslayer"
+  const val MOD_NAME = "MineSlayer"
+  const val VERSION = "alpha-1.0.0"
+
+  /**
+   * This is the first initialization event. Register tile entities here.
+   * The registry events below will have fired prior to entry to this method.
+   */
+  @Mod.EventHandler
+  fun preinit(event: FMLPreInitializationEvent) {
+
+  }
+
+  /**
+   * This is the second initialization event. Register custom recipes
+   */
+  @Mod.EventHandler
+  fun init(event: FMLInitializationEvent) {
+
+  }
+
+  /**
+   * This is the final initialization event. Register actions from other mods here
+   */
+  @Mod.EventHandler
+  fun postinit(event: FMLPostInitializationEvent) {
+
+  }
+
+  /**
+   * This is a special class that listens to registry events, to allow creation of mod blocks and items at the proper time.
+   */
+  @Mod.EventBusSubscriber
+  object ObjectRegistryHandler {
+    /**
+     * Listen for the register event for creating custom items
+     */
+    @SubscribeEvent
+    fun addItems(event: RegistryEvent.Register<Item>) {
+      /*
+      event.registry.register(ItemBlock(MySpecialBlock).setRegistryName(MOD_ID, "myBlock"))
+      event.registry.register(MySpecialItem.setRegistryName(MOD_ID, "mySpecialItem"))
+      */
+    }
 
     /**
-     * This is the first initialization event. Register tile entities here.
-     * The registry events below will have fired prior to entry to this method.
+     * Listen for the register event for creating custom blocks
      */
-    @Mod.EventHandler
-    fun preinit(event: FMLPreInitializationEvent) {
-
+    @SubscribeEvent
+    fun addBlocks(event: RegistryEvent.Register<Block>) {
+      /*
+      event.registry.register(MySpecialBlock.setRegistryName(MOD_ID, "mySpecialBlock"))
+      */
     }
+  }
 
-    /**
-     * This is the second initialization event. Register custom recipes
-     */
-    @Mod.EventHandler
-    fun init(event: FMLInitializationEvent) {
+  /* EXAMPLE ITEM AND BLOCK - you probably want these in separate files
+  object MySpecialItem : Item() {
 
-    }
+  }
 
-    /**
-     * This is the final initialization event. Register actions from other mods here
-     */
-    @Mod.EventHandler
-    fun postinit(event: FMLPostInitializationEvent) {
+  object MySpecialBlock : Block() {
 
-    }
-
-    /**
-     * This is a special class that listens to registry events, to allow creation of mod blocks and items at the proper time.
-     */
-    @Mod.EventBusSubscriber
-    object ObjectRegistryHandler {
-        /**
-         * Listen for the register event for creating custom items
-         */
-        @SubscribeEvent
-        fun addItems(event: RegistryEvent.Register<Item>) {
-            /*
-            event.registry.register(ItemBlock(MySpecialBlock).setRegistryName(MOD_ID, "myBlock"))
-            event.registry.register(MySpecialItem.setRegistryName(MOD_ID, "mySpecialItem"))
-            */
-        }
-
-        /**
-         * Listen for the register event for creating custom blocks
-         */
-        @SubscribeEvent
-        fun addBlocks(event: RegistryEvent.Register<Block>) {
-            /*
-            event.registry.register(MySpecialBlock.setRegistryName(MOD_ID, "mySpecialBlock"))
-            */
-        }
-    }
-
-    /* EXAMPLE ITEM AND BLOCK - you probably want these in separate files
-    object MySpecialItem : Item() {
-
-    }
-
-    object MySpecialBlock : Block() {
-
-    }
-    */
+  }
+  */
 }
